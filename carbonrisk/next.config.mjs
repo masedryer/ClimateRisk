91a33e737ceb892d7c72988193f15133a62ed1d9
Edry Rezal <123053274+masedryer@users.noreply.github.com>
2024-11-24 01:13:43 +0800

    Update next.config.mjs

--- carbonrisk/next.config.mjs
+++ carbonrisk/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    srcDir: "src", // Specify the 'src' directory as the root for your pages
+  },
+  reactStrictMode: true, // Optional: Enable React Strict Mode
+};
 
 export default nextConfig;
