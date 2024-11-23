0583376d0b19ee72a9bae02ca356b53fd76a1068
Edry Rezal <123053274+masedryer@users.noreply.github.com>
2024-11-24 01:27:50 +0800

    Update next.config.mjs

--- carbonrisk/next.config.mjs
+++ carbonrisk/next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true, // Enables the new app directory structure
   },
-  reactStrictMode: true, // Optional: Enable React Strict Mode
 };
 
 export default nextConfig;
