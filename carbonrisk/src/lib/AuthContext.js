5b44940217b221485706f76d57067d9fd90bdfe5
xjxcxb <22001027@myrp.edu.sg>
2025-02-01 18:08:10 +0800

    2
    2

--- carbonrisk/src/lib/AuthContext.js
+++ carbonrisk/src/lib/AuthContext.js
@@ -92,12 +92,17 @@ export const AuthProvider = ({ children }) => {
             throw error;
         }
     };
+
     const signInWithGoogle = async () => {
         try {
             const { data, error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: `${window.location.origin}/auth/callback`
+                    queryParams: {
+                        access_type: 'offline',
+                        prompt: 'consent',
+                    },
+                    redirectTo: 'https://ycimfxsnhtqakxqaknmj.supabase.co/auth/v1/callback'
                 }
             });
             
